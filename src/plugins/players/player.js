3cca23326c76f4d6df0758b9b7e43d5d7fcdbee0
seiyria <kyle@seiyria.com>
2016-06-23 16:21:55 -0500

    save after taking a turn

--- src/plugins/players/player.js
+++ src/plugins/players/player.js
@@ -1,11 +1,21 @@
 
 import _ from 'lodash';
 
+import { Logger } from '../../shared/logger';
+import { savePlayer } from './player.db';
+
 export class Player {
   constructor(opts) {
     _.extend(this, opts);
+
+    if(!this.name) Logger.error('Player', new Error('No name specified.'), opts);
   }
 
   takeTurn() {
+    this.save();
+  }
+
+  save() {
+    savePlayer(this);
   }
 }
