b468284fcdff11ce11016b526925788bd345569d
seiyria <kyle@seiyria.com>
2016-06-23 10:15:30 -0500

    remove extraneous console log

--- src/plugins/players/player.logout.js
+++ src/plugins/players/player.logout.js
@@ -4,7 +4,6 @@ import { removePlayer } from './player.worker';
 export const socket = (socket, worker) => {
 
   const logout = async () => {
-    console.log('authtoken', socket.getAuthToken());
     if(!socket.getAuthToken()) return;
     const { playerName } = socket.getAuthToken();
     removePlayer(worker, playerName);
