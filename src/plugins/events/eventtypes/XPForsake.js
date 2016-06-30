acede3c52f264d82428146c6bde5d97d4bf3ffc5
seiyria <kyle@seiyria.com>
2016-06-30 14:59:48 -0500

    adjust xp gain/loss % from events

--- src/plugins/events/eventtypes/XPForsake.js
+++ src/plugins/events/eventtypes/XPForsake.js
@@ -3,10 +3,10 @@ import { Event } from '../../../core/base/event';
 
 export const WEIGHT = 25;
 
-// Lose 5-15% XP
+// Lose 3-5% XP
 export class XPForsake extends Event {
   static operateOn(player) {
-    const xpMod = Math.floor(Event.chance.integer({ min: player._xp.maximum * 0.05, max: player._xp.maximum * 0.15 }));
+    const xpMod = Math.floor(Event.chance.integer({ min: player._xp.maximum * 0.03, max: player._xp.maximum * 0.05 }));
     const eventText = this.eventText('forsakeXp', player, { xp: xpMod });
 
     this.emitMessage({ affected: [player], eventText: `${eventText} [-${xpMod}xp]` });
