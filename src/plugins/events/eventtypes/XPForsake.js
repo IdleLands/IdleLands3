8688cd664fa2e8ae22cac5fced678a247c62c74e
seiyria <kyle@seiyria.com>
2016-07-19 09:43:52 -0500

    adjust probabilities of xp events

--- src/plugins/events/eventtypes/XPForsake.js
+++ src/plugins/events/eventtypes/XPForsake.js
@@ -2,7 +2,7 @@
 import { Event } from '../../../core/base/event';
 import { MessageCategories } from '../../../shared/adventure-log';
 
-export const WEIGHT = 65;
+export const WEIGHT = 5;
 
 // Lose 3-5% XP
 export class XPForsake extends Event {
