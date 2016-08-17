cc52a68411fef6d5def4b5afe9a8c33a0658b3a3
seiyria <kyle@seiyria.com>
2016-08-17 08:53:34 -0500

    fix dead people targetting

--- src/plugins/combat/spelltargetstrategy.js
+++ src/plugins/combat/spelltargetstrategy.js
@@ -4,18 +4,21 @@ import _ from 'lodash';
 export class SpellTargetStrategy {
   static allEnemies(caster) {
     return _(caster.$battle.allPlayers)
+      .reject(p => p.hp === 0)
       .reject(p => p.party === caster.party)
       .value();
   }
 
   static randomEnemy(caster) {
     return [_(caster.$battle.allPlayers)
+      .reject(p => p.hp === 0)
       .reject(p => p.party === caster.party)
       .sample()];
   }
 
   static allAllies(caster) {
     return _(caster.$battle.allPlayers)
+      .reject(p => p.hp === 0)
       .filter(p => p.party === caster.party)
       .value();
   }
