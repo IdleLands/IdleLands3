18478ae8f8c1723bc1c6dbc6a13f72f7f03e1594
seiyria <kyle@seiyria.com>
2016-08-18 23:33:41 -0500

    change reqs for coward to point to correct stat

--- src/plugins/personalities/personalities/Coward.js
+++ src/plugins/personalities/personalities/Coward.js
@@ -5,6 +5,6 @@ export class Coward extends Personality {
   static description = 'Your cowardice allows you to avoid combat more often.';
 
   static hasEarned(player) {
-    return player.$statistics.getStat('Character.Combat.Lose') >= 25;
+    return player.$statistics.getStat('Combat.Lose') >= 25;
   }
 }
