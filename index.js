d3a78456c8c9419037ef66251d81006c489e5e46
Si Dawson <si@sidawson.com>
2016-09-06 21:07:53 +1200

    check for env file
    just checks for an .env file at startup. Alerts user and exits if not
    found (better than the confusing error messages that otherwise spew out)

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ if(process.env.NODE_ENV === 'production' && !process.env.IGNORE_NEWRELIC) {
   require('newrelic');
 }
 
+var fs = require('fs');
+try {
+    fs.accessSync('./.env', fs.F_OK);
+} catch (e) {
+    console.log('Can\'t find the .env file. Please place one in the current dir');
+    process.exit();
+}
+
 process.on('uncaughtException', e => console.error(e));
 process.on('unhandledRejection', reason => console.error(reason));
 
