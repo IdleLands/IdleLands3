00c43607ec80af43cf2643a1307519fc74f5eaa1
Si Dawson <si@sidawson.com>
2016-09-09 12:29:10 +1200

    prepare git for push to production
    trim .gitignore so git->heroic will grab the dist directory. For test
    (right now) just dump a basic file into /dist. Later, can run babel
    command from here too

---
+++ scripts/prodprep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# change to root
+cd ..
+
+# obviously we could also put our babel command in here, to create the dist subtree, thusly:
+#babel -q --compact true --minified -d dist src
+
+# but for now...
+# put a temp file in there, for the /hello endpoint (so we know it's all working)
+echo 'hello' dist/hello.txt
+
+# delete the dist line from .gitignore, so git->heroku will auto pick up the dist dir
+sed -i '' '/dist/d' .gitignore
+
