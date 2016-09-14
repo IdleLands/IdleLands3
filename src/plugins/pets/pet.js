24b8c8061613393511a74e0190655ff71fe23be7
seiyria <kyle@seiyria.com>
2016-09-13 21:58:44 -0500

    fix console.log

--- src/plugins/pets/pet.js
+++ src/plugins/pets/pet.js
@@ -192,7 +192,6 @@ export class Pet extends Character {
   }
 
   canGainXp() {
-    console.log(this.level, this.$ownerRef.name, this.$ownerRef.level);
     return this.level < this.$ownerRef.level;
   }
 
