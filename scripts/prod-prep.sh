3d95d1548a4bac248fdb282908b43cad2ebaba0b
Si Dawson <si@sidawson.com>
2016-09-20 15:10:05 +1200

    work around silly heroku issue
    for whatever reason, heroku is saying /test.js doesn’t exist (it’s in
    the repo - not copying across? who knows). Just go full into the
    official babel test instead.

--- scripts/prod-prep.sh
+++ scripts/prod-prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # obviously we could also put our babel command in here, to create the dist subtree, thusly:
-#babel -q --compact true --minified -d dist src
+babel -q --compact true --minified -d /app/dist /app/src
 
 # put a temp file in there, for the /hello endpoint (so we know it's all working)
-cp /app/test.js /app/dist/
+# cp /app/test.js /app/dist/
