df64b76d8375c8805c0f0b5c4560f22503730cdb
Si Dawson <si@sidawson.com>
2016-09-20 15:38:30 +1200

    try to see what babel is doing

--- scripts/prod-prep.sh
+++ scripts/prod-prep.sh
@@ -8,5 +8,9 @@ ls
 # obviously we could also put our babel command in here, to create the dist subtree, thusly:
 babel -q --compact true --minified -d dist src
 
+ls src/
+
+ls dist/
+
 # put a temp file in there, for the /hello endpoint (so we know it's all working)
 # cp /app/test.js /app/dist/
