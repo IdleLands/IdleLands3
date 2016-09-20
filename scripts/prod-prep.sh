70a228cb7e6223a5353b362986f3535ece52e4f9
Si Dawson <si@sidawson.com>
2016-09-20 15:24:13 +1200

    more tweaks
    try to check we’re in the right dir

--- scripts/prod-prep.sh
+++ scripts/prod-prep.sh
@@ -2,8 +2,11 @@
 
 npm install --save babel-cli
 
+# just here while we figure out where the hell everything is
+ls
+
 # obviously we could also put our babel command in here, to create the dist subtree, thusly:
-babel -q --compact true --minified -d /app/dist /app/src
+babel -q --compact true --minified -d dist src
 
 # put a temp file in there, for the /hello endpoint (so we know it's all working)
 # cp /app/test.js /app/dist/
