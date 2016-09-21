ac08032c7e6e9d59b0bdffc051ede9c04ee5b1e3
seiyria <kyle@seiyria.com>
2016-09-21 16:52:29 -0500

    pull out updater function

--- src/shared/data-updater.js
+++ src/shared/data-updater.js
@@ -3,8 +3,10 @@ export const DataUpdater = (playerName, type, data) => {
   // Would initialise server with testing if imported on top.
   const primus = require('../primus/server').primus;
 
-  primus.forEach(spark => {
+  const updater = (spark) => {
     if(!spark.authToken || spark.authToken.playerName !== playerName) return;
     spark.write({ data, update: type });
-  });
+  };
+
+  primus.forEach(updater);
 };
