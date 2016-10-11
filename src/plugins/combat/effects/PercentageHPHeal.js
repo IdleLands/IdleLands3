a14a784a688bb95fc017d74c67107311a20927ae
Kenneth Shen <KennethShen@users.noreply.github.com>
2016-10-11 12:37:19 -0700

    Effect tick fixes
    * Effects don't tick after the player is dead. Closes IdleLands/IdleLands#470
    * Litany Of Pain and Percentage HP Heal tick as intended.

--- src/plugins/combat/effects/PercentageHPHeal.js
+++ src/plugins/combat/effects/PercentageHPHeal.js
@@ -3,6 +3,7 @@ import { Effect } from '../effect';
 
 export class PercentageHPHeal extends Effect {
   tick() {
+    super.tick();
     const healedHp = Math.round(this.target._hp.maximum * this.potency/100);
     this.target._hp.add(healedHp);
     this._emitMessage(this.target, `%player was healed for ${healedHp} hp by %casterName's %spellName!`);
