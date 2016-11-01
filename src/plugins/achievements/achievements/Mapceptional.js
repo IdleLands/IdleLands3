a9866a46cfac251f59e8d725769ff637b351d5ed
seiyria <kyle@seiyria.com>
2016-11-01 14:57:34 -0500

    closes #204

--- src/plugins/achievements/achievements/Mapceptional.js
+++ src/plugins/achievements/achievements/Mapceptional.js
@@ -10,7 +10,14 @@ export class Mapceptional extends Achievement {
 
     const validMaps = [
       { name: 'Norkos Secret -1', rewards: { con: 20 } },
-      { name: 'Norkos Dungeon -11', rewards: { con: 100 } }
+      { name: 'Dark Tower +1', rewards: { con: 25 } },
+      { name: 'Hall of Heroes', rewards: { con: 100 } },
+      { name: 'Norkos Dungeon -11', rewards: { con: 100 } },
+      { name: 'Fate Lake', rewards: { con: 500 } },
+      { name: 'Fate Pools -2', rewards: { con: 50 } },
+      { name: 'The Astral Plane', rewards: { con: 1000 } },
+      { name: 'The Elemental Plane -3', rewards: { con: 750 } },
+      { name: 'Merchant\'s Guild', rewards: { con: 250 } }
     ];
 
     return _.compact(_.map(validMaps, mapData => {
