5e7cfdb22d8d63bf01a14c31bd88835243fa4501
seiyria <kyle@seiyria.com>
2016-11-08 14:39:45 -0600

    fix xpblessparty xp gain, closes #526

--- src/plugins/events/events/XPBlessParty.js
+++ src/plugins/events/events/XPBlessParty.js
@@ -10,11 +10,14 @@ export class XPBlessParty extends Event {
   static WEIGHT = WEIGHT;
 
   static operateOn(player) {
+    const member = _(player.party.players)
+      .sortBy(['level'])
+      .head();
     const percent = Event.chance.floating({ fixed: 5, min: 0.01, max: 0.03 });
-    const xpMod = Math.floor(player._xp.maximum * percent);
+    const xpMod = Math.floor(member._xp.maximum * percent);
     const eventText = this.eventText('blessXpParty', player, { xp: xpMod, partyName: player.party.name });
 
-    this.emitMessage({ affected: player.party.players, eventText: `${eventText} [+${xpMod} xp, ~${(percent*100).toFixed(2)}%]`, category: MessageCategories.XP });
+    this.emitMessage({ affected: player.party.players, eventText: `${eventText} [+${xpMod} xp, ~${(percent*100).toFixed(2)}%|${member.name}]`, category: MessageCategories.XP });
 
     _.each(player.party.players, member => {
       member.gainXp(xpMod, false);
