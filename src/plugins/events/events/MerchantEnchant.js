2a5250d1943d875c5094b38e2483275a5e85f9ad
seiyria <kyle@seiyria.com>
2017-02-15 07:45:47 -0600

    change message order for overenchant

--- src/plugins/events/events/MerchantEnchant.js
+++ src/plugins/events/events/MerchantEnchant.js
@@ -40,9 +40,9 @@ export class MerchantEnchant extends Event {
     const choice = _.find(player.choices, { id });
     if(player.gold < choice.extraData.cost) return false;
     player.gainGold(-choice.extraData.cost, false);
-    Enchant.operateOn(player);
     player.$statistics.incrementStat('Character.Gold.Spent', choice.extraData.cost);
     this.emitMessage({ affected: [player], eventText: choice.extraData.eventText, category: MessageCategories.GOLD });
+    Enchant.operateOn(player);
   }
 
   static feedback(player) {
