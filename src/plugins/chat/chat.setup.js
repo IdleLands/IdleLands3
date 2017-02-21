2ef4994389c3fc7ca80c40c1db808073c5697e23
seiyria <kyle@seiyria.com>
2017-02-20 20:20:25 -0600

    extchat attempt again

--- src/plugins/chat/chat.setup.js
+++ src/plugins/chat/chat.setup.js
@@ -2,7 +2,7 @@
 import { SETTINGS } from '../../static/settings';
 
 export const chatSetup = (primus) => {
-  if(process.env.INSTANCE_NUMBER !== 0) return;
+  if(process.env.INSTANCE_NUMBER != 0) return;
   if(!SETTINGS.externalChat) return;
   if(primus.extChat) return;
 
