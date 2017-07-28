bb6acf5ec61b310ffec06006a07a20a69372467f
Kyle J. Kemp <kyle@seiyria.com>
2017-07-28 09:33:52 -0500

    Update perks.js

--- src/plugins/premium/perks.js
+++ src/plugins/premium/perks.js
@@ -1,21 +1,21 @@
 
 export const perks = [
-  { name: '+15% XP (1 day)', cost: 1000,
+  { name: '+15% XP (1 day)', cost: 5000,
     description: 'Give the world +15% XP for 1 day.',
     festivalDuration: 24,
     festivalData: { xp: 0.15 } },
 
-  { name: '+15% XP (1 week)', cost: 5000,
+  { name: '+15% XP (1 week)', cost: 25000,
     description: 'Give the world +15% XP for 1 week.',
     festivalDuration: 24 * 7,
     festivalData: { xp: 0.15 } },
 
-  { name: '+10% Item Find (1 day)', cost: 4000,
+  { name: '+10% Item Find (1 day)', cost: 3000,
     description: 'Give the world +10% Item Find for 1 day.',
     festivalDuration: 24,
     festivalData: { itemFindRangeMultiplier: 0.10 } },
 
-  { name: '+10% Item Find (1 week)', cost: 20000,
+  { name: '+10% Item Find (1 week)', cost: 15000,
     description: 'Give the world +10% Item Find for 1 week.',
     festivalDuration: 24 * 7,
     festivalData: { itemFindRangeMultiplier: 0.10 } },
@@ -25,22 +25,22 @@ export const perks = [
     festivalDuration: 24,
     festivalData: { str: 0.1, dex: 0.1, con: 0.1, int: 0.1, agi: 0.1 } },
 
-  { name: '+20% Luck (1 day)', cost: 3000,
+  { name: '+20% Luck (1 day)', cost: 5000,
     description: 'Give the world +20% luck for 1 day.',
     festivalDuration: 24,
     festivalData: { luk: 0.2 } },
 
-  { name: '+30% Salvage (1 day)', cost: 1500,
+  { name: '+30% Salvage (1 day)', cost: 3500,
     description: 'Give the world +30% salvage for 1 day.',
     festivalDuration: 24,
     festivalData: { salvage: 3 } },
 
-  { name: '+70% Salvage (1 day)', cost: 3000,
+  { name: '+70% Salvage (1 day)', cost: 7000,
     description: 'Give the world +70% salvage for 1 day.',
     festivalDuration: 24,
     festivalData: { salvage: 7 } },
 
-  { name: '+100% Salvage (1 day)', cost: 4500,
+  { name: '+100% Salvage (1 day)', cost: 12000,
     description: 'Give the world +100% salvage for 1 day.',
     festivalDuration: 24,
     festivalData: { salvage: 10 } },
