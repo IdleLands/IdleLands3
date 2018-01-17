93437c00eddec5e94923a25557a9f3ffcc3a29df
JukeDragh <jukedragh@gmail.com>
2018-01-16 20:08:49 -0600

    Adding Intimidate for Barbarians (#801)
    * Create Intimidate.js
    
    * Create Intimidate.js
    
    * Update Intimidate.js

---
+++ src/plugins/combat/effects/Intimidate.js
@@ -0,0 +1,12 @@
+import { Effect } from '../effect';
+
+export class Intimidate extends Effect {
+  affect() {
+    this.setStat(target, 'aegis', -this.potency);
+    this._emitMessage(this.target, '%casterName is getting up in %player\'s face!');
+  }
+
+  tick() {
+    this._emitMessage(this.target, '%casterName is getting up in %player\'s face!');
+  }
+}
