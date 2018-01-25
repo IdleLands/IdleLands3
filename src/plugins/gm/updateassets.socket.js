1e52c87ad4a0af898f01510c1c147cb87c746449
Kyle Kemp <kyle@seiyria.com>
2018-01-25 07:38:44 -0600

    add manual reboot command

---
+++ src/plugins/gm/updateassets.socket.js
@@ -0,0 +1,31 @@
+
+import { GameState } from '../../core/game-state';
+import { GMCommands } from './commands';
+import { Logger } from '../../shared/logger';
+
+export const event = 'plugin:gm:updateassets';
+export const description = 'Mod only. Update the assets then reboot.';
+export const args = '';
+export const socket = (socket) => {
+
+  const updateassets = async() => {
+    if(!socket.authToken) return;
+
+    const { playerName } = socket.authToken;
+
+    const player = GameState.getInstance().getPlayer(playerName);
+    if(!player || !player.isMod) return;
+    Logger.info('Socket:GM:UpdateAssets', `${playerName} (${socket.address.ip}) updating assets.`);
+
+    require('child_process').exec('node install-assets', (e) => {
+      if(e) {
+        console.error(e);
+        return;
+      }
+
+      process.exit(0);
+    });
+  };
+
+  socket.on(event, updateassets);
+};
