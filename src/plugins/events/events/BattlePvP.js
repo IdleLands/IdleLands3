747f8e615d7f9327c53c46300e6462fc7a9241aa
Kyle J. Kemp <kyle@seiyria.com>
2018-03-01 07:17:50 -0600

    Update BattlePvP.js

--- src/plugins/events/events/BattlePvP.js
+++ src/plugins/events/events/BattlePvP.js
@@ -11,7 +11,7 @@ import { MessageCategories } from '../../../shared/adventure-log';
 import { Logger } from '../../../shared/logger';
 import { SETTINGS } from '../../../static/settings';
 
-export const WEIGHT = 54;
+export const WEIGHT = 3;
 
 // Create a pvp battle
 export class BattlePvP extends Event {
